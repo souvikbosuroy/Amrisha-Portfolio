88f6c75d251a427fb09eadc1616613f2935ec715
webskitters <webskitters@webskitterss-iMac-Pro-9.local>
2025-06-23 13:35:00 +0530

    lenis_effect

--- script.js
+++ script.js
@@ -32,18 +32,19 @@ gridSlide.forEach((gridItem, index) => {
     });
 });
 
-const lenis = new Lenis({
-    duration: 1.2,      // speed of the scroll
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easing function
-    smooth: true,
-    smoothTouch: false,
-    direction: 'vertical',
-  });
+window.addEventListener("load", () => {
+      const lenis = new Lenis({
+        duration: 1.2,
+        easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smooth: true,
+        smoothTouch: false
+      });
 
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
 
-  requestAnimationFrame(raf);
+      requestAnimationFrame(raf);
+    });
 });
